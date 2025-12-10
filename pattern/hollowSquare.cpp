771b33ad7544c92dba59e6002cf0fb1ea83f17ad
Shivam Kumar <shivam108ku@gmail.com>
2025-12-10 15:22:33 +0530

    Refactor code structure for improved readability and maintainability

---
+++ pattern/hollowSquare.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void triangle( int n ){
+    for (int row=1; row<=n; row++){
+          for (int col = 1; col<=row; col++){
+             cout << "*";
+          }
+          cout << endl;
+    }
+    
+}
+  
+int main(){
+    
+    int n = 5;
+    int m = 5;
+    triangle(n);
+
+}
