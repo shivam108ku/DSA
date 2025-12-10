543158e960dd90a8dfc7553371550b1048a33f4b
Shivam Kumar <shivam108ku@gmail.com>
2025-12-10 15:50:35 +0530

    Refactor hollowSquare function and remove unused triangle function

--- pattern/hollowSquare.cpp
+++ pattern/hollowSquare.cpp
@@ -1,20 +1,41 @@
 #include<iostream>
 using namespace std;
 
-void triangle( int n ){
-    for (int row=1; row<=n; row++){
-          for (int col = 1; col<=row; col++){
-             cout << "*";
-          }
-          cout << endl;
-    }
+// void triangle( int n ){
+//     for (int row=1; row<=n; row++){
+//           for (int col = 1; col<=row; col++){
+//              cout << "*";
+//           }
+//           cout << endl;
+//     }
     
+// }
+
+void hollowSquare( int n  ){
+    for (int row=1 ; row<=n; row++){
+          if( row==1 || row==n){
+              for(int col=1; col<=n; col++){
+                cout << "*";
+              }
+          }
+          else {
+            cout << "*";
+            for(int col=1; col<=n-2; col++){
+                cout << " ";
+            }
+            cout << "*";
+          }
+          cout << endl;           
+    }
+    cout << endl;
 }
+     
+    
   
 int main(){
     
     int n = 5;
     int m = 5;
-    triangle(n);
+    hollowSquare(n );
 
 }
