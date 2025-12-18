94b51aaa9859467315d2b70323b301a6995280f3
Shivam Kumar <shivam108ku@gmail.com>
2025-12-18 10:48:51 +0530

    Refactor vector initialization to remove size specification and improve clarity

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 int main(){
 
-    vector<int> it(5);
+    vector<int> it;
     vector<int> miles(10);
     vector<int> distance(15 , 0); 
 
    // cout << *(it.begin());
+    
    it.push_back(10);
    it.push_back(20);
    it.push_back(30);
    it.push_back(40);
    it.push_back(50);
 
-   
+
+//    for(int i: it){
+//     cout << i << " ";
+//    }
     
 //    it.insert(it.begin(), 50) ;
 //    cout << it.at(0); 
