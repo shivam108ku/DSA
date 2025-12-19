d92d20aed301599414b467aaa628aaf40a8a6578
Shivam Kumar <shivam108ku@gmail.com>
2025-12-19 11:15:08 +0530

    using map functions

---
+++ stl/map.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <map>
+#include <unordered_map>
+using  namespace std;
+
+
+int main(){
+    
+    // creation
+    unordered_map<string , string>table;    
+
+    table["in"] = "India";
+    table.insert(make_pair("en","England"));
+
+    unordered_map<string, string>::iterator it = table.begin();
+
+    while(it != table.end()){
+        cout << it->first << " : " << it->second << endl;
+        it++;
+    }
+
+    cout << table.size();
+}
