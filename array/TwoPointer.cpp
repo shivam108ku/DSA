8e3dfd9897493308d247c6b115e8d1d703f72a96
Shivam Kumar <shivam108ku@gmail.com>
2025-12-22 20:40:11 +0530

    cange

--- array/TwoPointer.cpp
+++ array/TwoPointer.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[] = {0, 1, 0, 3, 12};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    int j = 0;  // non-zero position
+    int j = 0; 
 
     for (int i = 0; i < n; i++) {
         if (arr[i] != 0) {
