a7360310a5ed83a1cc0632192d5f6e7d3c11188c
Shivam Kumar <shivam108ku@gmail.com>
2025-12-23 17:09:50 +0530

    trying

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -1,1 +1,15 @@
-// Prgramming techniques 
+#include<iostream>
+using namespace std;
+
+int recfact(int val){
+    if(n==1 || n== 0){
+        return;
+    }
+    recFact( n * (n-1));
+}
+
+
+int main(){
+    int fact = 5;
+    recFact(fact);
+}
