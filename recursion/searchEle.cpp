bf37061f02bdb7503930cd0f3101e568d85ef542
Shivam Kumar <shivam108ku@gmail.com>
2026-01-02 11:53:27 +0530

    fix searchEle function to return -1 for not found and update main for correct output

--- recursion/searchEle.cpp
+++ recursion/searchEle.cpp
@@ -4,19 +4,24 @@ using namespace std;
 
 int searchEle(vector<int>& arr , int i , int ele){
     if(i >= arr.size()-1){
-        return 0;
+        return -1;
     }
     if(arr[i] == ele){
         return ele;
-    }else{
-        return - 1;
     }
     searchEle(arr, i+1 , ele);
-    cout << ele;
+    
 }
 
 int main(){
     vector<int>arr = {1,34,5,6,78,8,};
     int ele = 78;
-    searchEle(arr, 0 , ele);
+    int ans = searchEle(arr, 0 , ele);
+
+    if(ans != -1){
+        cout << "Element Found " << ans;
+    }
+    else{
+        cout << "Not Found";
+    }
 }
